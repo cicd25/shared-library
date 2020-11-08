9af3c5b543b4d4644bd33267e742780a70b6f655
cicd25 <cicd25@gmail.com>
2020-11-08 21:22:45 +0530

    business logic in src

---
+++ src/Addition.groovy
@@ -0,0 +1,13 @@
+package com.devops.jenkins.demo
+
+class Addition {
+
+    Addition() {
+
+    }
+
+    def add(int x, int y) {
+        return x+y
+    }
+
+}
---
+++ vars/add.groovy
@@ -0,0 +1,9 @@
+import com.devops.jenkins.demo.Addition
+
+def call(int x, int y) {
+
+    def addition = new Addition()
+    addition.add(x,y)
+
+
+}
