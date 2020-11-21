dc028d82a6ed48f37438f0448c7fd9475d3b4ea3
cicd25 <cicd25@gmail.com>
2020-11-21 20:17:54 +0530

    updated the add function

--- vars/add.groovy
+++ vars/add.groovy
@@ -1,7 +1,9 @@
 def call(int x, int y) {
 
-    def addition = new com.devops.jenkins.demo.Addition()
-    addition.add(x,y)
+    	def addition = new com.devops.jenkins.demo.Addition()
+    	def sum = addition.add(x,y)
 
-
+	println("sum of the two variables is : $sum")
+		
+	return sum
 }
